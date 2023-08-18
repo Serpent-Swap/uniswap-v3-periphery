b0bb0e2d9a17746a3a87d532207b5c07a9778dc3
Mark Ibanez <mark.ibanez@gmail.com>
2023-08-18 20:49:40 +0800

    Rename output file

--- wagmi.config.ts
+++ wagmi.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@wagmi/cli';
 import { hardhat } from '@wagmi/cli/plugins';
 
 export default defineConfig({
-  out: 'wagmi/uniswap-v3-core.ts',
+  out: 'wagmi/uniswap-v3-periphery.ts',
   contracts: [],
   plugins: [
     hardhat({
