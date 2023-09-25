b19df601695cbeff98081b20e6bb8e2f042fafb9
Mark Ibanez <mark.ibanez@gmail.com>
2023-09-25 14:46:39 +0800

    Update wrong pool factory address for sepolia

--- scripts/deploy-quoterV2.ts
+++ scripts/deploy-quoterV2.ts
@@ -7,7 +7,7 @@ const main = async () => {
     deployArgs.push('0x71992849909a5Ed0c8Cc3928F5F5287B13d08aBA'); // pool factory
     deployArgs.push('0x8049c9E3cE496b47E0fE8aa8EdAEf751cF87e07d'); // wxrp (weth9)
   } else if (network.name === 'sepolia') {
-    deployArgs.push('0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD'); // pool factory (uniswap deployment)
+    deployArgs.push('0x0227628f3F023bb0B980b67D528571c95c6DaC1c'); // pool factory (uniswap deployment)
     deployArgs.push('0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9'); // weth (weth9)
   }
 
