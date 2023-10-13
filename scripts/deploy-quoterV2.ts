38901cd945d7475a4d141771ffa767360536c855
Mark Ibanez <mark.ibanez@gmail.com>
2023-10-13 19:30:34 +0800

    Update pool factory addresses

--- scripts/deploy-quoterV2.ts
+++ scripts/deploy-quoterV2.ts
@@ -4,10 +4,10 @@ const main = async () => {
   const deployArgs: any[] = [];
 
   if (network.name === 'xrplDevnet') {
-    deployArgs.push('0x71992849909a5Ed0c8Cc3928F5F5287B13d08aBA'); // pool factory
+    deployArgs.push('0x123b5c88Cc98f8f73b686cd8bdEA213EAa4360e8'); // pool factory
     deployArgs.push('0x8049c9E3cE496b47E0fE8aa8EdAEf751cF87e07d'); // wxrp (weth9)
   } else if (network.name === 'sepolia') {
-    deployArgs.push('0x0227628f3F023bb0B980b67D528571c95c6DaC1c'); // pool factory (uniswap deployment)
+    deployArgs.push('0x19f217A65E827f6fA955F89b7ABfACcCcf7c9860'); // pool factory (uniswap deployment)
     deployArgs.push('0x7b79995e5f793A07Bc00c21412e50Ecae098E7f9'); // weth (weth9)
   }
 
